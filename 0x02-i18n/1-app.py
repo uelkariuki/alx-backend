7277b7b2dee1849a8a678020f4b9bd927653f9b2
uelkariuki <cratzuel@gmail.com>
2024-01-09 17:17:25 +0300

    Fix Pycodestyle errors

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,32 +6,34 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config(object):
     """class Config"""
 
     LANGUAGES = ["en", "fr"]
-    
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """set locale to english"""
     return 'en'
 
+
 @babel.timezoneselector
 def get_timezone():
     """Set timezone"""
     return 'UTC'
 
+
 @app.route('/')
 def index():
     """index method"""
-    return render_template('0-index.html')
-
-
-
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
