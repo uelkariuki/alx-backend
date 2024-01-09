039e6e6e03654fea9dea5f578e0f083038607aba
uelkariuki <cratzuel@gmail.com>
2024-01-09 15:05:25 +0300

    Setup a Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,5 +12,6 @@ def hello():
     """Hello method"""
     return render_template('0-index.html')
 
-if  __name__ == "__main__":
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
